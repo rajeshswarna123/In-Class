f9b2e4bfcc68b91de296ef68aa14d3e30b708376
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-02-28 14:06:21 -0500

    Added template using vitejs

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup la
+  components: { Messages },
+  components: { Nav },ng="ts">
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue';
+
+</script>
+
+<template>
+  <Nav >
+  <h1 class="title">
+    Praying Ukraine - Somthing Something
+  </h1>
+  <Messages >
+</template>
+
+<style>
+</style>
