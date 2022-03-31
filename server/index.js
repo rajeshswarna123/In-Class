782c3842a2b14977f39d05fa751cb88769a54299
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-03-31 14:16:30 -0400

    using env variables for port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const res = require('express/lib/response')
 const usersController = require('./controllers/users')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
+console.log(process.env);
 
 app
     // .use('/', express.static('server/public/'))
