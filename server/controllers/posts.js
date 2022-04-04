ff04bc4c194545c507f8c79a21fd521d03336e56
rajeshswarna123 <rajeshswarna123@gmail.com>
2022-04-04 18:19:55 +0000

    Deploying to node-deploy from @ rajeshswarna123/In-Class@47e8ce850a13906256e1b46f433f151315d6de5c 🚀

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,34 +1,39 @@
-
-const express = require("express");
-const { use } = require("express/lib/application");
-const res = require("express/lib/response");
+const express = require('express');
 const app = express.Router();
 
-const userModel = require(`../models/user`)
+const postModel = require('../models/post');
 
 const CREATED_STATUS = 201;
 
 app
     .get('/', (req, res) => {
-        // res.send(userModel.list());
-        res.send(userModel.list);
+        res.send(postModel.list);
     })
-    .get('/:id', (req, res)=>{
-        const user = userModel.get(req.params.id);
-        res.send(user)
+    .get('/:id', (req, res) => {
+
+        const post = postModel.get(req.params.id);
+        res.send(post);
 
     })
     .post('/', (req, res) => {
-        const user = userModel.create(req.body);
-        res.status(CREATED_STATUS).send(user);
+        const post = postModel.create(req.body);
+        res.status(CREATED_STATUS).send(post);
     })
-    .delete('/:id', (req, res)=>{
-        const user = userModel.remove(req.params.id);
-        res.send({sucess: true, errors: [], data: user});
+    .delete('/:id', (req, res) => {
+
+        const post = postModel.remove(req.params.id);
+
+        res.send({ success: true, errors: [], data: post });
+
     })
-    .patch('/:id', (req, res)=>{
-        const user = userModel.update(req.params.id, req.body);
-        res.send({sucess: true, errors: [], data: user});
+    .patch('/:id', (req, res) => {
+
+        const post = postModel.update(req.params.id, req.body );
+
+        res.send({ success: true, errors: [], data: post });
+
     })
+
+
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const res = require('express/lib/response')
 
 const usersController = require('./controllers/users')
+const postsController = require('./controllers/posts');
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app
         res.send('You are at the root of the API. For the best class ever - '+ process.env.BEST_CLASS_EVER)
     })
     .use('/api/users', usersController)
+    .use('/api/posts', postsController)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
